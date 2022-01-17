2270c8ec28074efffd5616fc7c2ec66bf6ce8b6b
mdanielbelarmino <daniel_belarmino@ymail.com>
2022-01-17 10:33:54 +0800

    updated arduino code

--- src/arduino/rover_makibot_v3_based_2/rover_makibot_v3_based_2.ino
+++ src/arduino/rover_makibot_v3_based_2/rover_makibot_v3_based_2.ino
@@ -8,20 +8,24 @@
 #include <geometry_msgs/Vector3Stamped.h>
 #include <ros/time.h>
 
-#define pwm_pinL 9
-#define dir_pinL 17
+#define pwm_pinR2 9
+#define dir_pinR2 17
 #define pwm_pinR 11
 #define dir_pinR 23
 #define pwm_pinL2 8
 #define dir_pinL2 16
-#define pwm_pinR2 10
-#define dir_pinR2 22
+#define pwm_pinL 10
+#define dir_pinL 22
 
 #define front_tof_pin A0
 #define rear_tof_pin A1
 
 #define MAX_SPEED 24
 #define LOOPTIME 100
+#define MAX_PWM 80
+#define ROT_SPEED 15
+
+#define prop 0.05
 
 int t2 = 0;
 int t1;
@@ -48,7 +52,9 @@ int16_t pwmR = 0;
 int16_t pwmL2 = 0;
 int16_t pwmR2 = 0;
 
-double gen_speed = 15;
+// Add direction parameter here
+
+double gen_speed = MAX_SPEED;
 
 void isrL() //interrupt service routine
 {
@@ -85,10 +91,10 @@ void messageCb( const geometry_msgs::Twist& cmd)
     des_spdR = gen_speed;
     des_spdL2 = gen_speed;
     des_spdR2 = gen_speed;
-    digitalWrite(dir_pinL, HIGH);
-    digitalWrite(dir_pinR, LOW);
-    digitalWrite(dir_pinL2, HIGH);
-    digitalWrite(dir_pinR2, LOW);
+    digitalWrite(dir_pinL, HIGH); // forward
+    digitalWrite(dir_pinR, LOW); // forward
+    digitalWrite(dir_pinL2, HIGH); // forward
+    digitalWrite(dir_pinR2, LOW); // forward
   }
   else if (spd < 0)
   {
@@ -101,41 +107,28 @@ void messageCb( const geometry_msgs::Twist& cmd)
     digitalWrite(dir_pinL2, LOW);
     digitalWrite(dir_pinR2, HIGH);
   }
-//  else if (ang > 0 )
-//  {
-//    des_spdL = 0;
-//    des_spdR = gen_speed;
-//    digitalWrite(dir_pinL, HIGH);
-//    digitalWrite(dir_pinR, LOW);
-//  }
-//  else if ( ang < 0 )
-//  {
-//    des_spdL = gen_speed;
-//    des_spdR = 0;
-//    digitalWrite(dir_pinL, HIGH);
-//    digitalWrite(dir_pinR, LOW);
-//  }
-  else if (ang > 0 )
+
+  else if (ang < 0 )
   {
-    des_spdL = gen_speed;
-    des_spdR = gen_speed;
-    des_spdL2 = gen_speed;
-    des_spdR2 = gen_speed;
-    digitalWrite(dir_pinL, LOW);
-    digitalWrite(dir_pinR, LOW);
-    digitalWrite(dir_pinL2, LOW);
-    digitalWrite(dir_pinR2, LOW);
-  }
-  else if ( ang < 0 )
-  {
-    des_spdL = gen_speed;
-    des_spdR = gen_speed;
-    des_spdL2 = gen_speed;
-    des_spdR2 = gen_speed;
+    des_spdL = ROT_SPEED;
+    des_spdR = ROT_SPEED;
+    des_spdL2 = ROT_SPEED;
+    des_spdR2 = ROT_SPEED;
     digitalWrite(dir_pinL, HIGH);
     digitalWrite(dir_pinR, HIGH);
     digitalWrite(dir_pinL2, HIGH);
     digitalWrite(dir_pinR2, HIGH);
+  }
+  else if ( ang > 0 )
+  {
+    des_spdL = ROT_SPEED;
+    des_spdR = ROT_SPEED;
+    des_spdL2 = ROT_SPEED;
+    des_spdR2 = ROT_SPEED;
+    digitalWrite(dir_pinL, LOW);
+    digitalWrite(dir_pinR, LOW);
+    digitalWrite(dir_pinL2, LOW);
+    digitalWrite(dir_pinR2, LOW);
   }
   else if (spd == 0)
   {
@@ -144,31 +137,9 @@ void messageCb( const geometry_msgs::Twist& cmd)
     des_spdL2 = 0;
     des_spdR2 = 0;
   }
-  //  else if (String(received_command) == "ROT_LEFT")
-  //  {
-  //    des_spdL = gen_speed;
-  //    des_spdR = gen_speed;
-  //    digitalWrite(dir_pinL, LOW);
-  //    digitalWrite(dir_pinR, LOW);
-  //  }
-  //  else if (String(received_command) == "ROT_RIGHT")
-  //  {
-  //    des_spdL = gen_speed;
-  //    des_spdR = gen_speed;
-  //    digitalWrite(dir_pinL, HIGH);
-  //    digitalWrite(dir_pinR, HIGH);
-  //  }
-  //  else if (String(received_command) == "FRONT_PROXIMITY_Q")
-  //  {
-  //    Serial.println(front_tof);
-  //  }
-  //  else if (String(received_command) == "REAR_PROXIMITY_Q")
-  //  {
-  //    Serial.println(rear_tof);
-  //  }
 }
 
-ros::Subscriber<geometry_msgs::Twist> sub("/turtle1/cmd_vel", &messageCb );
+ros::Subscriber<geometry_msgs::Twist> sub("/cmd_vel", &messageCb );
 geometry_msgs::Vector3Stamped speed_msg;                                //create a "speed_msg" ROS message
 ros::Publisher speed_pub("speed", &speed_msg);                          //create a publisher to ROS topic "speed" using the "speed_msg" type
 
@@ -179,10 +150,11 @@ void setup()
   nh.subscribe(sub);
   nh.advertise(speed_pub);
 
-  attachInterrupt(4, isrL, RISING);
+
+  attachInterrupt(4, isrR2, RISING);
   attachInterrupt(2, isrR, RISING);
   attachInterrupt(5, isrL2, RISING);
-  attachInterrupt(3, isrR2, RISING); 
+  attachInterrupt(3, isrL, RISING);
 
   pinMode(pwm_pinL, OUTPUT);
   pinMode(dir_pinL, OUTPUT);
@@ -199,38 +171,41 @@ void setup()
   analogWrite(pwm_pinL, 0);
   analogWrite(pwm_pinR2, 0);
   analogWrite(pwm_pinL2, 0);
-  digitalWrite(dir_pinL, HIGH);
-  digitalWrite(dir_pinR, LOW);
-  digitalWrite(dir_pinL2, HIGH);
-  digitalWrite(dir_pinR2, LOW);
+
+  digitalWrite(dir_pinL, HIGH); // forward
+  digitalWrite(dir_pinR, LOW); // forward
+  digitalWrite(dir_pinL2, HIGH); // forward
+  digitalWrite(dir_pinR2, LOW); // forward
+
 }
 
 int ctr = 0;
 
 void loop()
 {
-  detachInterrupt(4);           //detaches the interrupt pin 19
-  detachInterrupt(2);           //detaches the interrupt pin 21
-  detachInterrupt(5);           //detaches the interrupt pin 18
-  detachInterrupt(3);           //detaches the interrupt pin 20
+
+  detachInterrupt(4);           //detaches the interrupt pin 19 L old
+  detachInterrupt(2);           //detaches the interrupt pin 21 R
+  detachInterrupt(5);           //detaches the interrupt pin 18 L2
+  detachInterrupt(3);           //detaches the interrupt pin 20 R2
 
   t1 = millis();    //finds the time
   bool front_tof = digitalRead(front_tof_pin);
   bool rear_tof = digitalRead(rear_tof_pin);
 
   period = t1 - t2;
-  if (period == 100)
+  if (period >= 10)
   {
 
     float lp = pulseL;
     float rp = pulseR;
     float lp2 = pulseL2;
     float rp2 = pulseR2;
-    
-    rpmL = (lp / 4.3) * 60;
-    rpmR = (rp / 4.3) * 60;
-    rpmL2 = (lp2 / 4.3) * 60;
-    rpmR2 = (rp2 / 4.3) * 60;
+
+    rpmL = (lp / 0.43) * 60;
+    rpmR = (rp / 0.43) * 60;
+    rpmL2 = (lp2 / 0.43) * 60;
+    rpmR2 = (rp2 / 0.43) * 60;
 
     curr_spdL = (rpmL / 60) * (2 * PI * 0.08) * 100;
     curr_spdR = (rpmR / 60) * (2 * PI * 0.08) * 100;
@@ -244,22 +219,22 @@ void loop()
 
     t2 = t1;           //saves the current time
     int errL = des_spdL - curr_spdL;
-    pwmL = pwmL + (errL * 0.2); // proportional
+    pwmL = pwmL + (errL * prop); // proportional
 
     int errR = des_spdR - curr_spdR;
-    pwmR = pwmR + (errR * 0.2); // proportional
+    pwmR = pwmR + (errR * prop); // proportional
 
     int errL2 = des_spdL2 - curr_spdL2;
-    pwmL2 = pwmL2 + (errL2 * 0.2); // proportional
+    pwmL2 = pwmL2 + (errL2 * prop); // proportional
 
     int errR2 = des_spdR2 - curr_spdR2;
-    pwmR2 = pwmR2 + (errR2 * 0.2); // proportional
+    pwmR2 = pwmR2 + (errR2 * prop); // proportional
 
-    pwmL = (pwmL > 255) ? 255 : pwmL;
-    pwmR = (pwmR > 255) ? 255 : pwmR;
-    pwmL2 = (pwmL2 > 255) ? 255 : pwmL2;
-    pwmR2 = (pwmR2 > 255) ? 255 : pwmR2;
-    
+    pwmL = (pwmL > MAX_PWM) ? MAX_PWM : pwmL;
+    pwmR = (pwmR > MAX_PWM) ? MAX_PWM : pwmR;
+    pwmL2 = (pwmL2 > MAX_PWM) ? MAX_PWM : pwmL2;
+    pwmR2 = (pwmR2 > MAX_PWM) ? MAX_PWM : pwmR2;
+
     pwmL = (pwmL < 0) ? 0 : pwmL;
     pwmR = (pwmR < 0) ? 0 : pwmR;
     pwmL2 = (pwmL2 < 0) ? 0 : pwmL2;
@@ -269,13 +244,23 @@ void loop()
     analogWrite(pwm_pinR, pwmR);      //sets the desired speed
     analogWrite(pwm_pinL2, pwmL2);      //sets the desired speed
     analogWrite(pwm_pinR2, pwmR2);      //sets the desired speed
+
+    if(des_spdL == 0) 
+      pwmL = 0;
+    if(des_spdL2 == 0) 
+      pwmL2 = 0;
+    if(des_spdR == 0) 
+      pwmR = 0;
+    if(des_spdR2 == 0) 
+      pwmR2 = 0;
+
     publishSpeed(LOOPTIME);
   }
 
-  attachInterrupt(4, isrL, RISING);
+  attachInterrupt(4, isrR2, RISING);
   attachInterrupt(2, isrR, RISING);
   attachInterrupt(5, isrL2, RISING);
-  attachInterrupt(3, isrR2, RISING);
+  attachInterrupt(3, isrL, RISING);
 
   nh.spinOnce();
 
@@ -285,9 +270,9 @@ void loop()
 //Publish function for odometry, uses a vector type message to send the data (message type is not meant for that but that's easier than creating a specific message type)
 void publishSpeed(double time) {
   speed_msg.header.stamp = nh.now();      //timestamp for odometry data
-  speed_msg.vector.x = curr_spdL;    //left wheel speed (in m/s)
-  speed_msg.vector.y = curr_spdR;   //right wheel speed (in m/s)
-  speed_msg.vector.z = time / 1000;       //looptime, should be the same as specified in LOOPTIME (in s)
+  speed_msg.vector.x = pwmL;    //left wheel speed (in m/s)
+  speed_msg.vector.y = pwmL2;   //right wheel speed (in m/s)
+  speed_msg.vector.z = des_spdL; //time / 1000;       //looptime, should be the same as specified in LOOPTIME (in s)
   speed_pub.publish(&speed_msg);
   //  nh.loginfo("Publishing odometry");
 }
